e48e06425d4a765a8c3f51296acd8f3a778813c9
Alessio Vulpinari <alessiovulpinari99@gmail.com>
2024-06-07 11:51:28 +0200

    pom and initial setups
    dependencies, pom.xml setups and persistence.xml

--- src/main/java/alessiovulpinari/Application.java
+++ src/main/java/alessiovulpinari/Application.java
@@ -1,6 +1,11 @@
 package alessiovulpinari;
 
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
 public class Application {
+
+    private static final EntityManagerFactory emf = Persistence.createEntityManagerFactory("u1-w3-d5-Java");
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
